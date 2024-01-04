96d7bb5093ce6b5e0489f746a796708e1f274393
bricksky <skydh1214@naver.com>
2024-01-04 20:36:04 +0900

    단어 길이 재기

---
+++ 2024_01/2743.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string word;
+
+    cin >> word;
+    cout << word.size();
+
+    return 0;
+}
