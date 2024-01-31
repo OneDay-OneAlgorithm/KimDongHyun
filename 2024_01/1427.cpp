5c0e4d9cdaa8c8733840d0eb9acc355201c2e028
bricksky <skydh1214@naver.com>
2024-01-31 19:22:43 +0900

    소트인사이드

--- 2024_01/1427.cpp
+++ 2024_01/1427.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <algorithm>
+#include <functional>
+
 using namespace std;
 
 int main()
 {
-    string n;
-    cin >> n;
-    sort(n.begin(), n.end());
-    cout << n;
+    string str;
+    cin >> str;
+
+    sort(str.begin(), str.end(), greater<char>());
+    cout << str;
 }
