ebdc1a7522c1be0cb95b9270080b646f3261272d
bricksky <skydh1214@naver.com>
2024-02-07 20:21:04 +0900

    이칙연산

---
+++ 2024_01/15726.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a, b, c, x, y;
+    cin >> a >> b >> c;
+    x = (double)(a * b) / c;
+    y = ((double)a / b) * c;
+    if (x > y)
+        cout << x;
+    else
+        cout << y;
+    return 0;
+}
